90ecae1297beb3ff00afe24dad2752b1139d3003
Abdellah Es-Soudati <abdellahes-soudati@Abdellahs-Air.fritz.box>
2023-03-14 16:15:38 +0100

    new firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase : {
-    apiKey: "AIzaSyAwmzp5PekuxKWLbsT7I3xS0swmW6xdWaw",
-    authDomain: "todo-42605.firebaseapp.com",
-    projectId: "todo-42605",
-    storageBucket: "todo-42605.appspot.com",
-    messagingSenderId: "334829010388",
-    appId: "1:334829010388:web:103102ceb8a0fea4fe2a41",
-    measurementId: "G-341LHH8YHY"
+    apiKey: "AIzaSyDcjEcIpaq8Q_3aek4SmG34zh7PgEbJ60I",
+    authDomain: "staffmanager-9ba41.firebaseapp.com",
+    projectId: "staffmanager-9ba41",
+    storageBucket: "staffmanager-9ba41.appspot.com",
+    messagingSenderId: "885836488",
+    appId: "1:885836488:web:7d3302a88bb6fb84e533fd",
+    measurementId: "G-SGK4BE7H0H"
   },
     production: true
   };
