0a26cb27a4777afc727167aa90157897cd655088
Abiess <essoudati@hotmail.de>
2023-12-01 20:47:27 +0100

    testme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,30 @@
 
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+
+// module.exports = {
+//   content: ['./src/index.html', './src/**/*.{html,ts}'], // Add any other paths that include Tailwind CSS classes in your project
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms'), // Enable tailwindcss/forms plugin
+//   ],
+// }
 
 module.exports = {
-  content: ['./src/index.html', './src/**/*.{html,ts}'], // Add any other paths that include Tailwind CSS classes in your project
+  purge: {
+    enabled: process?.argv?.indexOf("build") !== -1,
+    content: [
+      "./src/**/*.html",
+      "./src/**/*.ts",
+      "./projects/**/*.html",
+      "./projects/**/*.ts"
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'), // Enable tailwindcss/forms plugin
-  ],
-}
-
+  variants: {},
+  plugins: [],
+ }
