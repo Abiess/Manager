1890f7766ca0d8adcde6f8017f958668ccd999f8
Abiess <essoudati@hotmail.de>
2023-12-10 15:41:04 +0100

    change app domain

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -12,9 +12,9 @@ export const environment = {
     production: false,
     firebase : {
       apiKey: "AIzaSyDcjEcIpaq8Q_3aek4SmG34zh7PgEbJ60I",
-      authDomain: "staffmanager-9ba41.firebaseapp.com",
+      authDomain: "mada-digital.com",
       projectId: "staffmanager-9ba41",
-      storageBucket: "staffmanager-9ba41.appspot.com",
+      storageBucket: "mada-digital.com",
       messagingSenderId: "885836488",
       appId: "1:885836488:web:7d3302a88bb6fb84e533fd",
       measurementId: "G-SGK4BE7H0H"
