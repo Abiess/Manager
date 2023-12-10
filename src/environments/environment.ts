e85535d791be4c6be20592e4191f9ad67fc80df2
Abiess <essoudati@hotmail.de>
2023-12-10 15:31:08 +0100

    dd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase : {
     apiKey: "AIzaSyDcjEcIpaq8Q_3aek4SmG34zh7PgEbJ60I",
-    authDomain: "staffmanager-9ba41.firebaseapp.com",
-    projectId: "staffmanager-9ba41",
-    storageBucket: "staffmanager-9ba41.appspot.com",
-    messagingSenderId: "885836488",
-    appId: "1:885836488:web:7d3302a88bb6fb84e533fd",
-    measurementId: "G-SGK4BE7H0H"
+      authDomain: "staffmanager-9ba41.firebaseapp.com",
+      projectId: "staffmanager-9ba41",
+      storageBucket: "staffmanager-9ba41.appspot.com",
+      messagingSenderId: "885836488",
+      appId: "1:885836488:web:7d3302a88bb6fb84e533fd",
+      measurementId: "G-SGK4BE7H0H"
   },
     production: true
   };
