7f739058da0d92f16f42ea5c4d1fe2caa90333fa
Abiess <essoudati@hotmail.de>
2023-12-21 19:18:27 +0100

    dddddd

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -12,9 +12,9 @@ export const environment = {
     production: false,
     firebase : {
       apiKey: "AIzaSyDcjEcIpaq8Q_3aek4SmG34zh7PgEbJ60I",
-      authDomain: "mada-digital.com",
+      authDomain: "staffmanager-9ba41.firebaseapp.com",
       projectId: "staffmanager-9ba41",
-      storageBucket: "mada-digital.com",
+      storageBucket: "staffmanager-9ba41.appspot.com",
       messagingSenderId: "885836488",
       appId: "1:885836488:web:7d3302a88bb6fb84e533fd",
       measurementId: "G-SGK4BE7H0H"
